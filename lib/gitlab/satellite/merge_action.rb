b1dbda58d63ba2227cb266b7d9fcba0467b10612
Douwe Maan <douwe@gitlab.com>
2015-07-23 13:46:36 +0200

    Resolve conflict with Satellite::MergeAction.

--- lib/gitlab/satellite/merge_action.rb
+++ lib/gitlab/satellite/merge_action.rb
@@ -46,14 +46,7 @@ module Gitlab
             merge_repo.git.push(default_options, :origin, merge_request.target_branch)
 
             # remove source branch
-<<<<<<< HEAD
             remove_source_branch(merge_repo)
-=======
-            if merge_request.remove_source_branch?
-              # will raise CommandFailed when push fails
-              merge_repo.git.push(default_options, :origin, ":#{merge_request.source_branch}")
-            end
->>>>>>> 7-13-stable
             # merge, push and branch removal successful
             true
           end
@@ -194,7 +187,7 @@ module Gitlab
       end
 
       def remove_source_branch(repo)
-        if merge_request.should_remove_source_branch && !project.root_ref?(merge_request.source_branch) && !merge_request.for_fork?
+        if merge_request.remove_source_branch?
           # will raise CommandFailed when push fails
           repo.git.push(default_options, :origin, ":#{merge_request.source_branch}")
         end
