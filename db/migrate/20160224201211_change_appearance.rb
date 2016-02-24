ad8d81e0b654e9557bbf6f8ab136fb0b8b7b86f4
Zeger-Jan van de Weg <zegerjan@gitlab.com>
2016-02-24 22:22:26 +0100

    Fix migration

--- db/migrate/20160224201211_change_appearance.rb
+++ db/migrate/20160224201211_change_appearance.rb
@@ -1,4 +1,9 @@
 class ChangeAppearance < ActiveRecord::Migration
   def change
+    change_table :appearances do |t|
+      t.remove :dark_logo
+
+      t.rename :light_logo, :header_logo
+    end 
   end
 end
