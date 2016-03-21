64f5e66338890c875c4596b60075eb617764d043
Rémy Coutable <remy@rymai.me>
2016-03-21 17:17:45 +0100

    Fix an issue with wiki in search results when using ES

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -880,6 +880,8 @@ class Repository
   def parse_search_result_from_elastic(result)
     ref = result["_source"]["blob"]["commit_sha"]
     filename = result["_source"]["blob"]["path"]
+    extname = File.extname(filename)
+    basename = filename.sub(/#{extname}$/, '')
     content = result["_source"]["blob"]["content"]
     total_lines = content.lines.size
 
@@ -909,6 +911,7 @@ class Repository
 
     OpenStruct.new(
       filename: filename,
+      basename: basename,
       ref: ref,
       startline: from + 1,
       data: data.join
