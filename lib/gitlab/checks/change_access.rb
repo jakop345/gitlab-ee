23432a24d6c75f6f8ef48e39b058d5bdd7663e8c
Valery Sizov <valery@gitlab.com>
2016-08-18 08:18:54 +0300

    Fix ChangeAccess

--- lib/gitlab/checks/change_access.rb
+++ lib/gitlab/checks/change_access.rb
@@ -12,7 +12,7 @@ module Gitlab
       end
 
       def exec
-        error = push_checks || tag_checks || protected_branch_checks || push_rule_check
+        error = push_checks || tag_checks || protected_branch_checks || push_rules_checks
 
         if error
           GitAccessStatus.new(false, error)
@@ -61,6 +61,18 @@ module Gitlab
         end
       end
 
+      def push_rules_checks
+        # Returns nil if all push rule checks passed successfully
+        # or the error if any hook fails
+        error = push_rule_check
+
+        if !error && license_allows_file_locks?
+          error = path_locks_check
+        end
+
+        error
+      end
+
       private
 
       def protected_tag?(tag_name)
@@ -76,32 +88,47 @@ module Gitlab
       end
 
       def push_rule_check
-        return unless @newrev && @oldrev
+        return unless project.push_rule && @newrev && @oldrev
 
         push_rule = project.push_rule
 
         # Prevent tag removal
         if Gitlab::Git.tag_name(@ref)
-          if push_rule.try(:deny_delete_tag) && protected_tag?(Gitlab::Git.tag_name(@ref)) && Gitlab::Git.blank_ref?(@newrev)
-            return  "You can not delete a tag"
+          if push_rule.deny_delete_tag && protected_tag?(Gitlab::Git.tag_name(@ref)) && Gitlab::Git.blank_ref?(@newrev)
+            "You can not delete tag"
           end
         else
-          commit_validation = push_rule.try(:commit_validation?)
-
           # if newrev is blank, the branch was deleted
-          return if Gitlab::Git.blank_ref?(@newrev) ||
-            !(commit_validation || validate_path_locks?)
+          return if Gitlab::Git.blank_ref?(@newrev) || !push_rule.commit_validation?
 
           commits.each do |commit|
             next if commit_from_annex_sync?(commit.safe_message)
 
-            if commit_validation
-              error = check_commit(commit, push_rule)
-              return error if error
-            end
-
-            if error = check_commit_diff(commit, push_rule)
+            if error = check_commit(commit, push_rule)
               return error
+            end
+          end
+        end
+
+        nil
+      end
+
+      def path_locks_check
+        return unless project.path_locks.any? && @newrev && @oldrev
+
+        # locks protect default branch only
+        return if project.default_branch != Gitlab::Git.branch_name(@ref)
+
+        commits.each do |commit|
+          next if commit_from_annex_sync?(commit.safe_message)
+
+          commit.raw_diffs.each do |diff|
+            path = diff.new_path || diff.old_path
+
+            lock_info = project.find_path_lock(path)
+
+            if lock_info && lock_info.user != user_access.user
+              return "The path '#{lock_info.path}' is locked by #{lock_info.user.name}"
             end
           end
         end
@@ -138,82 +165,34 @@ module Gitlab
           end
         end
 
+        if error = check_commit_diff(commit, push_rule)
+          return error
+        end
+
         nil
       end
 
       def check_commit_diff(commit, push_rule)
-        validations = validations_for_commit(commit, push_rule)
+        if push_rule.file_name_regex.present?
+          commit.raw_diffs.each do |diff|
+            if (diff.renamed_file || diff.new_file) && diff.new_path =~ Regexp.new(push_rule.file_name_regex)
+              return "File name #{diff.new_path.inspect} is prohibited by the pattern '#{push_rule.file_name_regex}'"
+            end
+          end
+        end
 
-        return if validations.empty?
+        if push_rule.max_file_size > 0
+          commit.raw_diffs.each do |diff|
+            next if diff.deleted_file
 
-        commit.raw_diffs(deltas_only: true).each do |diff|
-          validations.each do |validation|
-            if error = validation.call(diff)
-              return error
+            blob = project.repository.blob_at(commit.id, diff.new_path)
+            if blob && blob.size && blob.size > push_rule.max_file_size.megabytes
+              return "File #{diff.new_path.inspect} is larger than the allowed size of #{push_rule.max_file_size} MB"
             end
           end
         end
 
         nil
-      end
-
-      def validations_for_commit(commit, push_rule)
-        validations = base_validations
-
-        return validations unless push_rule
-
-        if push_rule.file_name_regex.present?
-          validations << file_name_validation(push_rule.file_name_regex)
-        end
-
-        if push_rule.max_file_size > 0
-          validations << file_size_validation(commit, push_rule.max_file_size)
-        end
-
-        validations
-      end
-
-      def base_validations
-        validate_path_locks? ? [path_locks_validation] : []
-      end
-
-      def validate_path_locks?
-        @validate_path_locks ||= license_allows_file_locks? &&
-          project.path_locks.any? && @newrev && @oldrev &&
-          project.default_branch == @branch_name # locks protect default branch only
-      end
-
-      def path_locks_validation
-        lambda do |diff|
-          path = diff.new_path || diff.old_path
-
-          lock_info = project.find_path_lock(path)
-
-          if lock_info && lock_info.user != user_access.user
-            return "The path '#{lock_info.path}' is locked by #{lock_info.user.name}"
-          end
-        end
-      end
-
-      def file_name_validation(file_name_regex)
-        regexp = Regexp.new(file_name_regex)
-
-        lambda do |diff|
-          if (diff.renamed_file || diff.new_file) && diff.new_path =~ regexp
-            return "File name #{diff.new_path.inspect} is prohibited by the pattern '#{file_name_regex}'"
-          end
-        end
-      end
-
-      def file_size_validation(commit, max_file_size)
-        lambda do |diff|
-          return if diff.deleted_file
-
-          blob = project.repository.blob_at(commit.id, diff.new_path)
-          if blob && blob.size && blob.size > max_file_size.megabytes
-            return "File #{diff.new_path.inspect} is larger than the allowed size of #{max_file_size} MB"
-          end
-        end
       end
 
       def commits
