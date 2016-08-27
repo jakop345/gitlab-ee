5e24660d2f0b373fd63a54b1459fa2113cd0796d
Ruben Davila <rdavila84@gmail.com>
2016-08-26 21:01:02 -0500

    Backport some changes from 02717c and 46f4b5
    Without these changes some ElasticSearch specs were failing.

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,4 +1,4 @@
 require 'webmock'
 require 'webmock/rspec'
 
-WebMock.disable_net_connect!(allow_localhost: true, allow: 'elasticsearch')
+WebMock.disable_net_connect!(allow_localhost: true, allow: 'registry.gitlab.com-gitlab-org-test-elastic-image')
