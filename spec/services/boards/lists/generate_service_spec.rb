741fd812ab39f40b3b2c0f5e126d4af069a96e7e
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2016-10-05 14:06:34 -0300

    Update Boards::Lists::GenerateService to generate for a specific board

--- spec/services/boards/lists/generate_service_spec.rb
+++ spec/services/boards/lists/generate_service_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 describe Boards::Lists::GenerateService, services: true do
   describe '#execute' do
-    let(:project) { create(:project_with_board) }
-    let(:board)   { project.board }
+    let(:project) { create(:empty_project) }
+    let(:board)   { create(:board, project: project) }
     let(:user)    { create(:user) }
 
     subject(:service) { described_class.new(project, user) }
 
     context 'when board lists is empty' do
       it 'creates the default lists' do
-        expect { service.execute }.to change(board.lists, :count).by(4)
+        expect { service.execute(board) }.to change(board.lists, :count).by(4)
       end
     end
 
@@ -18,13 +18,13 @@ describe Boards::Lists::GenerateService, services: true do
       it 'does not creates the default lists' do
         create(:list, board: board)
 
-        expect { service.execute }.not_to change(board.lists, :count)
+        expect { service.execute(board) }.not_to change(board.lists, :count)
       end
     end
 
     context 'when project labels does not contains any list label' do
       it 'creates labels' do
-        expect { service.execute }.to change(project.labels, :count).by(4)
+        expect { service.execute(board) }.to change(project.labels, :count).by(4)
       end
     end
 
@@ -33,7 +33,7 @@ describe Boards::Lists::GenerateService, services: true do
         create(:label, project: project, name: 'Development')
         create(:label, project: project, name: 'Ready')
 
-        expect { service.execute }.to change(project.labels, :count).by(2)
+        expect { service.execute(board) }.to change(project.labels, :count).by(2)
       end
     end
   end
