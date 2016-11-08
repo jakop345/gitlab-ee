28754e5cc38a99feb308b23e9f5c148c800853bb
Alejandro Rodríguez <alejorro70@gmail.com>
2016-11-08 18:38:16 -0300

    Allow `ENV['ELASTIC_HOST']` through WebMock

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,4 +1,7 @@
 require 'webmock'
 require 'webmock/rspec'
 
-WebMock.disable_net_connect!(allow_localhost: true, allow: 'registry.gitlab.com-gitlab-org-test-elastic-image')
+WebMock.disable_net_connect!(
+  allow_localhost: true,
+  allow: ['elasticsearch', ENV['ELASTIC_HOST'] || 'registry.gitlab.com-gitlab-org-test-elastic-image']
+)
